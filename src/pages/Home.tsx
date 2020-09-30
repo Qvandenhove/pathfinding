b7e86c571c39d19e0d860a95a932228967af5b98
QVandenhove <quentin.vandenhove@viacesi.fr>
2020-09-30 12:35:07 +0200

    room grid done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,18 +4,32 @@ import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 import { Grid } from 'pathfinding'
 
-const Home: React.FC = () => {
-  const longueur_canvas:number = 1200
-  const hauteur_canvas:number = 600
-  const hauteur:number = 60
-  const longueur:number = 60
-  let matrix:any = []
-  for(let X=0; X<hauteur;X++){
+const longueur_canvas:number = 1200
+const hauteur_canvas:number = 600
+const hauteur:number = 60
+const longueur:number = 60
+const room_number:number = 6
+const room_size:number = Math.round(longueur/room_number)
+let matrix:any = []
+  for(let Y=0; Y<hauteur;Y++){
     matrix.push([])
-    for(let Y=0; Y<longueur; Y++){
-      matrix[X].push(0)
+    for(let X=0; X<longueur; X++){
+      let isBuildingWall = (X === 0 || Y === 0 || X === longueur - 1 || Y === hauteur - 1 || X === hauteur/2 + 1 || X === hauteur/2 - 1)
+      let isBuildingDoor = (X === hauteur/2 && Y === 0)
+      let isRoomWall = ((Y % room_size === 0 && X !== hauteur/2))
+      let isRoomDoor = (Y % room_size === 1 && X !== 0 && Y!== 0 && X !== longueur - 1 && Y !== hauteur - 1)
+      if((isBuildingWall || isRoomWall) && (isRoomDoor === false && isBuildingDoor === false)){
+        matrix[Y].push(1)
+      }else{
+        matrix[Y].push(0)
+      }
     }
   }
+
+const Home: React.FC = () => {
+  
+  
+  
   let grid = new Grid(matrix)
   return (
     <IonPage>
@@ -36,7 +50,6 @@ const Home: React.FC = () => {
               let svg_line = line.map((cell:any, index_h:any) => {
                 return <rect x={index * (longueur_canvas/longueur)} y={index_h*(hauteur_canvas/hauteur)} width={longueur_canvas/longueur} height={hauteur_canvas/hauteur} stroke="black" stroke-width="1" fill={cell === 0? "white": "black"} />
               })
-              console.log(svg_line)
             return <Fragment>{svg_line}</Fragment>
             })}
           </svg>
