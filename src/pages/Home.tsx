4f22e2caa58d4a0d63f5dcd1d274ae82afdae2c6
QVandenhove <quentin.vandenhove@viacesi.fr>
2020-09-30 16:36:40 +0200

    traçage de chemin

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,36 +1,50 @@
 import { IonContent, IonHeader, IonItem, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
-import { Grid } from 'pathfinding'
+import { AStarFinder, Grid } from 'pathfinding'
+import helpers from '../helpers/helpers';
+let generation = false
 
-const longueur_canvas:number = 1200
-const hauteur_canvas:number = 600
-const hauteur:number = 60
-const longueur:number = 60
-const room_number:number = 6
-const room_size:number = Math.round(longueur/room_number)
-let matrix:any = []
-  for(let Y=0; Y<hauteur;Y++){
-    matrix.push([])
-    for(let X=0; X<longueur; X++){
-      let isBuildingWall = (X === 0 || Y === 0 || X === longueur - 1 || Y === hauteur - 1 || X === hauteur/2 + 1 || X === hauteur/2 - 1)
-      let isBuildingDoor = (X === hauteur/2 && Y === 0)
-      let isRoomWall = ((Y % room_size === 0 && X !== hauteur/2))
-      let isRoomDoor = (Y % room_size === 1 && X !== 0 && Y!== 0 && X !== longueur - 1 && Y !== hauteur - 1)
-      if((isBuildingWall || isRoomWall) && (isRoomDoor === false && isBuildingDoor === false)){
-        matrix[Y].push(1)
-      }else{
-        matrix[Y].push(0)
-      }
-    }
-  }
+function transpose(array:any) {
+  return array.reduce((prev:any, next:any) => next.map((item:any, i:any) =>
+      (prev[i] || []).concat(next[i])
+  ), []);
+}
 
 const Home: React.FC = () => {
+  const longueur_canvas:number = 800
+  const hauteur_canvas:number = 800
+  const hauteur:number = 60
+  const longueur:number = 60
+  var size = longueur_canvas/longueur
+  const room_number:number = 6
+  const room_size:number = Math.round(longueur/room_number)
+  let [matrix, setMatrix] = useState([])
+  let [svg_path_string, setSvgString] = useState("")
+  
+  helpers.createGrid(hauteur, longueur, 6).then((value) => {
+    console.log(generation)
+    if(generation === false){
+      generation = true
+      setMatrix(value)
+      let grid = new Grid(transpose(value))
+      let finder = new AStarFinder()
+      let path = finder.findPath(5,5,51,50, grid)
+      console.log(value)
+      console.log(path)
+      let svg_path = []
+      svg_path.push(`M${path[0][0] * size + size/2} ${path[0][1] * size + size/2}`)
+      for(let i = 1;i < path.length; i++){
+        svg_path.push(`L${path[i][0] * size + size/2} ${path[i][1] * size + size/2}`)
+      }
+      setSvgString(svg_path.join(" "))
+        
+    }
+  })
   
   
   
-  let grid = new Grid(matrix)
   return (
     <IonPage>
       <IonHeader>
@@ -48,10 +62,17 @@ const Home: React.FC = () => {
           <svg width={longueur_canvas} height={hauteur_canvas}>
             {matrix.map((line:any, index:number) => {
               let svg_line = line.map((cell:any, index_h:any) => {
-                return <rect x={index * (longueur_canvas/longueur)} y={index_h*(hauteur_canvas/hauteur)} width={longueur_canvas/longueur} height={hauteur_canvas/hauteur} stroke="black" stroke-width="1" fill={cell === 0? "white": "black"} />
+                let cell_value
+                if (cell === 0){
+                  cell_value = "white"
+                }else{
+                  cell_value = "black"
+                }
+                return <rect x={index * (longueur_canvas/longueur)} y={index_h*(hauteur_canvas/hauteur)} width={longueur_canvas/longueur} height={hauteur_canvas/hauteur} stroke="black" stroke-width="1" fill={cell_value} />
               })
             return <Fragment>{svg_line}</Fragment>
             })}
+            <path fill="none" stroke-width="3" stroke="#ffff00" d={svg_path_string} />
           </svg>
         </IonItem>
         
