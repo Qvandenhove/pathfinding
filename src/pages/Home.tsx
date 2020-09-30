9ed02889b253c3d341c42a9f35c4bde96c958ea5
QVandenhove <quentin.vandenhove@viacesi.fr>
2020-09-30 11:17:57 +0200

    building-grid

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,22 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
+import { IonContent, IonHeader, IonItem, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React, { Fragment } from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
+import { Grid } from 'pathfinding'
 
 const Home: React.FC = () => {
+  const longueur_canvas:number = 1200
+  const hauteur_canvas:number = 600
+  const hauteur:number = 60
+  const longueur:number = 60
+  let matrix:any = []
+  for(let X=0; X<hauteur;X++){
+    matrix.push([])
+    for(let Y=0; Y<longueur; Y++){
+      matrix[X].push(0)
+    }
+  }
+  let grid = new Grid(matrix)
   return (
     <IonPage>
       <IonHeader>
@@ -17,7 +30,18 @@ const Home: React.FC = () => {
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        <IonItem>
+          <svg width={longueur_canvas} height={hauteur_canvas}>
+            {matrix.map((line:any, index:number) => {
+              let svg_line = line.map((cell:any, index_h:any) => {
+                return <rect x={index * (longueur_canvas/longueur)} y={index_h*(hauteur_canvas/hauteur)} width={longueur_canvas/longueur} height={hauteur_canvas/hauteur} stroke="black" stroke-width="1" fill={cell === 0? "white": "black"} />
+              })
+              console.log(svg_line)
+            return <Fragment>{svg_line}</Fragment>
+            })}
+          </svg>
+        </IonItem>
+        
       </IonContent>
     </IonPage>
   );
